354a8946ff900025b25f2da3fbb66ecd0e77b747
Arjun singh gurjar <115486632+Arjun09091999@users.noreply.github.com>
2023-06-29 12:40:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function volume_sphere() {
-    //Write your code here
+// function volume_sphere() {
+//     //Write your code here
   
-} 
+// } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+// window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+function volume_sphere() {
+  const radius = parseFloat(document.getElementById("Radius").value);
+  const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+  document.getElementById("volume").value = volume.toFixed(2);
+  return false;  // Prevent form submission
+}
+
+window.onload = function() {
+  document.getElementById('MyForm').onsubmit = volume_sphere;
+};
